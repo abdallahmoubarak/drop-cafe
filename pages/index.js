50341ded92425f8fc7c8b19eaffb77f712f13b5c
Abdallah Moubarak <112470831+abdallahmoubarak@users.noreply.github.com>
2022-09-29 01:19:48 +0300

    setting the background in main page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,61 @@
-import Link from 'next/link'
+import Head from "next/head";
+import { useState } from "react";
 
 export default function IndexPage() {
+  const [lang, setLang] = useState("");
+  const [main, setMain] = useState(true);
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  )
+    <>
+      <div>
+        <Head>
+          <title>Drop Cafe</title>
+        </Head>
+      </div>
+      <style jsx global>{`
+        * {
+          margin: 0;
+          padding: 0;
+          -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+          text-decoration: unset;
+          outline: none;
+          -webkit-tap-highlight-color: transparent;
+          font-family: sans-serif;
+          -ms-scroll-chaining: none;
+          overscroll-behavior: contain;
+          -ms-overflow-style: none;
+          scrollbar-width: none;
+        }
+        *::-webkit-scrollbar {
+          display: none;
+        }
+        body {
+          background-image: -webkit-gradient(
+              linear,
+              left top,
+              left bottom,
+              from(black),
+              to(black)
+            ),
+            url("/img/headerimg.jpg");
+          background-image: -o-linear-gradient(black, black),
+            url("/img/headerimg.jpg");
+          background-image: linear-gradient(black, black),
+            url("/img/headerimg.jpg");
+          background-blend-mode: saturation;
+          background-attachment: fixed;
+          background-position: center center;
+          background-color: transparent;
+          font-size: 18px;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+            "Helvetica Neue", sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          z-index: -10;
+        }
+      `}</style>
+    </>
+  );
 }
